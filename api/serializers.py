2c59f14822f27b06583f087fe4c660d80d87bec7
xahgmah <xahgmah@yandex.ru>
2015-10-12 18:05:19 +0300

    create model for exam, fix view to model vieset, generate key

--- api/serializers.py
+++ api/serializers.py
@@ -1,49 +1,93 @@
+from collections import OrderedDict
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 import json
 from django.utils.translation import ugettext as _
+from rest_framework.fields import SkipField
+
+from models import Exam
 
 
 class JSONSerializerField(serializers.Field):
-    """ Serializer for JSONField -- required to make field writable"""
+    """ Serializer for orgExtraField"""
+    FIELD_LIST = [
+        'examStartDate',
+        'examEndDate',
+        'noOfStudents',
+        'examId',
+        'courseId',
+        'firstName',
+        'lastName',
+    ]
 
     def to_internal_value(self, data):
-        field_list = {
-            'examStartDate',
-            'examEndDate',
-            'noOfStudents',
-            'examID',
-            'courseID',
-            'firstName',
-            'lastName',
-        }
         json_data = {}
         try:
             json_data = json.loads(data)
-            if cmp(json_data.keys(), field_list) != 0:
+            if cmp(json_data.keys(), self.FIELD_LIST) != 0:
                 raise serializers.ValidationError(
                     _("orgExtra fields list incorrect"))
-
-        except ValueError, e:
+        except ValueError:
             raise serializers.ValidationError(
                 _("orgExtra field value error. Must be json"))
         finally:
             return json_data
 
-    def to_representation(self, value):
-        return value
+    def to_representation(self, instance):
+        """
+        Field value -> String.
+        """
+        result = {}
+        for field in self.FIELD_LIST:
+            result[field] = getattr(instance, field)
+        return result
 
 
-class RegisterExamAttemptSerializer(serializers.Serializer):
-    examCode = serializers.CharField(max_length=60)
-    organization = serializers.CharField(max_length=60)
-    duration = serializers.IntegerField()
-    reviewedExam = serializers.CharField(max_length=60)
-    reviewerNotes = serializers.CharField(max_length=60)
-    examPassword = serializers.CharField(max_length=60)
-    examSponsor = serializers.CharField(max_length=60)
-    examName = serializers.CharField(max_length=60)
-    ssiProduct = serializers.CharField(max_length=60)
-    orgExtra = serializers.CharField(
-        max_length=60,
+class ExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = ('examCode', 'organization', 'duration', 'reviewedExam',
+                  'reviewerNotes', 'examPassword', 'examSponsor',
+                  'examName', 'ssiProduct', 'orgExtra')
+
+    orgExtra = JSONSerializerField(
         style={'base_template': 'textarea.html'},
     )
+
+    def to_representation(self, instance):
+        """
+        Object instance -> Dict of primitive datatypes.
+        """
+        ret = OrderedDict()
+        fields = self._readable_fields
+
+        for field in fields:
+            try:
+                attribute = field.get_attribute(instance)
+            except SkipField:
+                continue
+            except AttributeError:
+                if isinstance(field, JSONSerializerField):
+                    ret[field.field_name] = field.to_representation(instance)
+                continue
+
+            if attribute is None:
+                ret[field.field_name] = None
+            else:
+                ret[field.field_name] = field.to_representation(attribute)
+
+        return ret
+
+    def validate(self, data):
+        '''
+        Data validation
+        :param data: data from post/put request
+        :return: clean data
+        '''
+        data.update(data['orgExtra'])
+        del (data['orgExtra'])
+        try:
+            Exam(**data).full_clean()
+        except ValidationError as e:
+            raise serializers.ValidationError(e.message_dict)
+        return super(ExamSerializer, self).validate(data)
