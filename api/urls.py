41004f92182ee14e14814960785d712060d7b369
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-11-05 01:53:15 +0200

    fix

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import include, url, patterns
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.routers import DefaultRouter
 from views_edx import ExamViewSet, APIRoot
-from views_ui import start_exam, poll_status, review
+from views_ui import start_exam, poll_status, Review
 
 router = DefaultRouter()
 router.register(r'exam_register', ExamViewSet,
@@ -16,7 +15,7 @@ urlpatterns = patterns(
         name='start_exam'),
     url(r'poll_status/(?P<attempt_code>[-\w]+)$', poll_status,
         name='poll_status'),
-    url(r'review/$', csrf_exempt(review),
+    url(r'review/$', Review.as_view(),
         name='review'),
     (r'^', include(router.urls)),
 
--- api/views_ui.py
+++ api/views_ui.py
@@ -2,10 +2,13 @@ import json
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
 from api.web_soket_methods import send_ws_msg
 from models import Exam
 from edx_api import start_exam_request, poll_status_request, \
     send_review_request
+from api.auth import CsrfExemptSessionAuthentication
 
 
 @api_view(['GET'])
@@ -41,89 +44,92 @@ def poll_status(request, attempt_code):
     return Response(data=data, status=response.status_code)
 
 
-@api_view(['POST'])
-def review(request):
-    """
-    Request example:
+class Review(APIView):
 
-    `{"attempt_code":"029288A9-9198-4C99-9D3F-E589695CF5D7"}`
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
 
-    """
-    passing_review_status = ['Clean', 'Rules Violation']
-    failing_review_status = ['Not Reviewed', 'Suspicious']
-    exam = get_object_or_404(Exam, exam_code=request.data.get('attempt_code'))
+    def post(self, request):
+        """
+        Request example:
 
-    review_payload = {
-        "examDate": "",
-        "examProcessingStatus": "Review Completed",
-        "examTakerEmail": "",
-        "examTakerFirstName": "John",
-        "examTakerLastName": "Doe",
-        "keySetVersion": "",
-        "examApiData": {
-            "duration": exam.duration,
-            "examCode": exam.exam_code,
-            "examName": exam.exam_name,
-            "examPassword": exam.exam_password,
-            "examSponsor": exam.exam_sponsor,
-            "examUrl": "http://localhost:8000/api/edx_proctoring/proctoring_launch_callback/start_exam/4d07a01a-1502-422e-b943-93ac04dc6ced",
-            "orgExtra": {
-                "courseID": exam.course_id,
-                "examEndDate": exam.exam_end_date,
-                "examID": exam.exam_id,
-                "examStartDate": exam.exam_start_date,
-                "noOfStudents": exam.no_of_students
+        `{"attempt_code":"029288A9-9198-4C99-9D3F-E589695CF5D7"}`
+
+        """
+        passing_review_status = ['Clean', 'Rules Violation']
+        failing_review_status = ['Not Reviewed', 'Suspicious']
+        exam = get_object_or_404(Exam, exam_code=request.data.get('attempt_code'))
+
+        review_payload = {
+            "examDate": "",
+            "examProcessingStatus": "Review Completed",
+            "examTakerEmail": "",
+            "examTakerFirstName": "John",
+            "examTakerLastName": "Doe",
+            "keySetVersion": "",
+            "examApiData": {
+                "duration": exam.duration,
+                "examCode": exam.exam_code,
+                "examName": exam.exam_name,
+                "examPassword": exam.exam_password,
+                "examSponsor": exam.exam_sponsor,
+                "examUrl": "http://localhost:8000/api/edx_proctoring/proctoring_launch_callback/start_exam/4d07a01a-1502-422e-b943-93ac04dc6ced",
+                "orgExtra": {
+                    "courseID": exam.course_id,
+                    "examEndDate": exam.exam_end_date,
+                    "examID": exam.exam_id,
+                    "examStartDate": exam.exam_start_date,
+                    "noOfStudents": exam.no_of_students
+                },
+                "organization": exam.organization,
+                "reviewedExam": exam.reviewed_exam,
+                "reviewerNotes": exam.reviewer_notes,
+                "ssiProduct": "rp-now"
             },
-            "organization": exam.organization,
-            "reviewedExam": exam.reviewed_exam,
-            "reviewerNotes": exam.reviewer_notes,
-            "ssiProduct": "rp-now"
-        },
-        "overAllComments": "",
-        "reviewStatus": passing_review_status[0],
-        "userPhotoBase64String": "",
-        "videoReviewLink": "",
-        "examMetaData": {
-            "examCode": request.data.get('attempt_code'),
-            "examName": exam.exam_name,
-            "examSponsor": exam.exam_sponsor,
-            "organization": exam.organization,
-            "reviewedExam": "True",
-            "reviewerNotes": "Closed Book",
-            "simulatedExam": "False",
-            "ssiExamToken": "",
-            "ssiProduct": "rp-now",
-            "ssiRecordLocator": exam.generate_key()
-        },
-        "desktopComments": [
-            {
-                "comments": "Browsing other websites",
-                "duration": 88,
-                "eventFinish": 88,
-                "eventStart": 12,
-                "eventStatus": "Suspicious"
+            "overAllComments": "",
+            "reviewStatus": passing_review_status[0],
+            "userPhotoBase64String": "",
+            "videoReviewLink": "",
+            "examMetaData": {
+                "examCode": request.data.get('attempt_code'),
+                "examName": exam.exam_name,
+                "examSponsor": exam.exam_sponsor,
+                "organization": exam.organization,
+                "reviewedExam": "True",
+                "reviewerNotes": "Closed Book",
+                "simulatedExam": "False",
+                "ssiExamToken": "",
+                "ssiProduct": "rp-now",
+                "ssiRecordLocator": exam.generate_key()
             },
-        ],
-        "webCamComments": [
-            {
-                "comments": "Photo ID not provided",
-                "duration": 796,
-                "eventFinish": 796,
-                "eventStart": 0,
-                "eventStatus": "Suspicious"
-            }
-        ]
-    }
+            "desktopComments": [
+                {
+                    "comments": "Browsing other websites",
+                    "duration": 88,
+                    "eventFinish": 88,
+                    "eventStart": 12,
+                    "eventStatus": "Suspicious"
+                },
+            ],
+            "webCamComments": [
+                {
+                    "comments": "Photo ID not provided",
+                    "duration": 796,
+                    "eventFinish": 796,
+                    "eventStart": 0,
+                    "eventStatus": "Suspicious"
+                }
+            ]
+        }
 
-    response = send_review_request(review_payload)
-    data = {
-        'hash': exam.generate_key(),
-        'status': ''
-    }
-    if response.status_code == 200:
-        data['status'] = 'review has sent'
-    else:
-        data['status'] = 'send review failed'
+        response = send_review_request(review_payload)
+        data = {
+            'hash': exam.generate_key(),
+            'status': ''
+        }
+        if response.status_code == 200:
+            data['status'] = 'review has sent'
+        else:
+            data['status'] = 'send review failed'
 
-    return Response(data=data,
-                    status=response.status_code)
+        return Response(data=data,
+                        status=response.status_code)
