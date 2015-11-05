95b16cc4c5b74cdd614f2f1852411177c59d387e
xahgmah <xahgmah@yandex.ru>
2015-11-05 18:25:28 +0200

    auth with accesstoken between fronted and backend

--- api/auth.py
+++ api/auth.py
@@ -1,6 +1,27 @@
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import SessionAuthentication, \
+    TokenAuthentication
+from social.apps.django_app.default.models import UserSocialAuth
+from rest_framework import exceptions
+from django.utils.translation import ugettext_lazy as _
+
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
 
     def enforce_csrf(self, request):
         return
+
+
+class SsoTokenAuthentication(TokenAuthentication):
+    model = UserSocialAuth
+
+    def authenticate_credentials(self, key):
+        try:
+            token = self.model.objects.select_related('user').get(extra_data={"access_token": key})
+            # token = self.model.objects.select_related('user').get(key=key)
+        except self.model.DoesNotExist:
+            raise exceptions.AuthenticationFailed(_('Invalid token.'))
+
+        if not token.user.is_active:
+            raise exceptions.AuthenticationFailed(_('User inactive or deleted.'))
+
+        return (token.user, token)
