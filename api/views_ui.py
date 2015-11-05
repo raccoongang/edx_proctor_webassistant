c86f3ee5db95bd59c954dcd6dfec87052914f64f
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-11-05 02:26:17 +0200

    fix

--- api/views_ui.py
+++ api/views_ui.py
@@ -129,7 +129,7 @@ class Review(APIView):
         if response.status_code == 200:
             data['status'] = 'review_was_sent'
         else:
-            data['status'] = 'send review failed'
+            data['status'] = response.json()['reason']
 
         return Response(data=data,
                         status=response.status_code)
