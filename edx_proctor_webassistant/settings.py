ac61280aa8c8331306e59a15526164765daa02b5
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-11-06 15:02:41 +0200

    fix

--- edx_proctor_webassistant/settings.py
+++ edx_proctor_webassistant/settings.py
@@ -148,6 +148,7 @@ BOWER_INSTALLED_APPS = (
 # Pipeline
 # PIPELINE_ENABLED = True
 PIPELINE_DISABLE_WRAPPER = True
+PIPELINE_YUGLIFY_BINARY = '/bin/true'
 PIPELINE_CSS = {
     'css': {
         'source_filenames': (
