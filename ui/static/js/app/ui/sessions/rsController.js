fcfdb73cb3cfac95ca413c46db0d1358d6854516
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-11-19 19:27:17 +0200

    update

--- ui/static/js/app/ui/sessions/rsController.js
+++ ui/static/js/app/ui/sessions/rsController.js
@@ -1,9 +1,26 @@
 (function(){
     angular.module('proctor').controller('SessionCtrl', function($scope, data){
-        $scope.courses = {};
-        $scope.exams = {};
+        $scope.courses = [];
+        $scope.exams = [];
+        $scope.session = {};
+        if (data.data.results !== undefined && data.data.results.length) {
+            var c_list = [];
+            angular.forEach(data.data.results, function (val, key) {
+                c_list.push({name: val.name, id: val.id});
+            });
+            $scope.courses = c_list;
+        }
 
-        console.log(data);
+        $scope.$watch('session.course', function(val, old){
+            if (data.data.results !== undefined && data.data.results.length) {
+                var e_list = $.grep(data.data.results, function(e){
+                    return e.id == val;
+                });
+                if (e_list.length){
+                    $scope.exams = e_list[0].proctored_exams;
+                }
+            }
+        });
 
         $scope.start_session = function(){
 
