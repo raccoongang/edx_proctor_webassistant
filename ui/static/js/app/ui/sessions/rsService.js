9da9cb05cf271ec0515c25dc9ec5e0a8b4f010fc
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-11-19 14:05:23 +0200

    updates

---
+++ ui/static/js/app/ui/sessions/rsService.js
@@ -0,0 +1,14 @@
+(function(){
+    angular.module('testEventSessions')
+        .service('TestSessions', function(){
+            var Session = {};
+
+            this.registerSession = function(){
+
+            };
+
+            this.endSession = function(){
+
+            };
+        });
+})();
