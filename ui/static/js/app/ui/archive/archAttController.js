ad0743ec88a9e5270caa5db86b9a2448963f5fed
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-12-06 23:30:58 +0200

    update

--- ui/static/js/app/ui/archive/archAttController.js
+++ ui/static/js/app/ui/archive/archAttController.js
@@ -1,5 +1,6 @@
 (function(){
     angular.module('proctor').controller('ArchAttCtrl', function($scope, sessions){
         $scope.sessions = sessions.data.results;
+        $scope.exam_name = $scope.sessions[0].examName;
     });
 })();
