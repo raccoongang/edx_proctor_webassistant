287b20b1d3e6a21854ab148834a71b5f382de32e
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-12-07 20:03:20 +0200

    update

--- ui/static/js/app/ui/archive/archAttController.js
+++ ui/static/js/app/ui/archive/archAttController.js
@@ -1,7 +1,7 @@
 (function(){
     angular.module('proctor').controller('ArchAttCtrl', function($scope, $filter, sessions){
         $scope.sessions = angular.copy(sessions.data.results);
-        $scope.search.filter = '';
+        $scope.search = {filter: ''};
         $scope.exam_name = $scope.sessions.length?$scope.sessions[0].examName:'';
 
         //$scope.$watch('search.filter', function(val){
