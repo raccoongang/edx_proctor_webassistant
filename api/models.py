fb65dd1ebc37e4e0d3beb28dbbe1e31254d0f2b3
xahgmah <xahgmah@yandex.ru>
2015-12-09 21:02:38 +0200

    inprogress event session proxy

--- api/models.py
+++ api/models.py
@@ -139,7 +139,7 @@ class InProgressEventSessionManager(models.Manager):
     def get_queryset(self):
         return super(InProgressEventSessionManager,
                      self).get_queryset().exclude(
-            status=EventSession.ARCHIVED)
+            status=EventSession.IN_PROGRESS)
 
 
 class ArchivedEventSessionManager(models.Manager):
