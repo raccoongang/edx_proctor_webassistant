fb442ae16c4b8e2d675f546e8479a45a1bf54b93
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-12-10 14:08:38 +0200

    update

--- ui/static/js/app/ui/home/hmController.js
+++ ui/static/js/app/ui/home/hmController.js
@@ -21,6 +21,13 @@
                     $location.path('/session');
                 }
 
+                $scope.test_list = [
+                    {name: "dfjfd", status: "sdfhsdhsd"},
+                    {name: "jdfsgjfs", status: "sdfhsfhsf"},
+                    {name: "xf4tw4", status: "rtyw45"},
+                    {name: "rethrh", status: "353636"}
+                ];
+
                 $scope.ws_data = [];
 
                 // get student exams from session
