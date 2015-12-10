54b8da56f90a242e61f880e13577a39aa07b8a1b
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-12-10 20:20:17 +0200

    update

--- ui/static/js/app/ui/profile/pfController.js
+++ ui/static/js/app/ui/profile/pfController.js
@@ -1,3 +1,10 @@
-/**
- * Created by hobo on 11/20/15.
- */
+(function(){
+    angular.module('proctor').controller('ProfileCtrl', function($scope){
+        $scope.data = [
+            {'name': "John", 'age': 25},
+            {'name': "Hanna", 'age': 5},
+            {'name': "Dick", 'age': 23},
+            {'name': "Carlos", 'age': 12}
+        ];
+    });
+})();
