73ef7a6df631714c90e6640b67efeb18bbb81b75
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-12-10 20:29:35 +0200

    update

--- ui/static/js/app/common/helpers.js
+++ ui/static/js/app/common/helpers.js
@@ -2,7 +2,7 @@ Array.prototype.in_array = function (val){
     return this.indexOf(val) >= 0;
 };
 
-Array.prototype.filter = function(params){
+Array.prototype._filter = function(params){
     return $.grep(this, function(e){
         var is = true;
         angular.forEach(params, function(val, key){
