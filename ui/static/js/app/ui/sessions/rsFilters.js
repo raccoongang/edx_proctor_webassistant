6ca93c64721132a25882aaf6c15c8784db5174e1
Tamoshaitis Vladas <amd.vladas@gmail.com>
2015-12-14 11:54:11 +0200

    update

--- ui/static/js/app/ui/sessions/rsFilters.js
+++ ui/static/js/app/ui/sessions/rsFilters.js
@@ -1,10 +1,12 @@
 'use strict';
 (function(){
     var app = angular.module('proctor');
+
     app.filter('hasaccess', function(){
         return function(arr) {
             var ret = [];
             angular.forEach(arr, function(val, key){
+                console.log(val.has_access, val.has_access === true);
                 ret.push(val.has_access === true);
             });
             return ret;
