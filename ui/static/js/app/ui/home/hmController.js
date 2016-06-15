3cb8165280da23ed97e1f108caeecdb65c333216
Vladas <hobo@Vladass-MacBook-Pro.local>
2016-06-15 21:42:51 +0300

    fixed save comments error

--- ui/static/js/app/ui/home/hmController.js
+++ ui/static/js/app/ui/home/hmController.js
@@ -146,11 +146,11 @@
                     attempt.comments.push(data);
                     Api.save_comment(attempt.examCode,
                         {
-                            "comments": data.comment,
+                            "comment": data.comment,
                             "duration": 1,
-                            "eventFinish": data.timestamp,
-                            "eventStart": data.timestamp,
-                            "eventStatus": data.status
+                            "event_finish": data.timestamp,
+                            "event_start": data.timestamp,
+                            "event_status": data.status
                         }
                     );
                 };
